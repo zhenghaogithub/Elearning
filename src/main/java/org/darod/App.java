6ed1ccac0dae10acb1628ad2d2c008bfa20b520d
Dakod <794470129@qq.com>
2019-06-19 18:32:22 +0800

    项目启动

---
+++ src/main/java/org/darod/App.java
@@ -0,0 +1,13 @@
+package org.darod;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
