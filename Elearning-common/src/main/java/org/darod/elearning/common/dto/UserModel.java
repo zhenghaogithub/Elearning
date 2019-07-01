bac3791d796edddfab492a56f389cb6b0b954399
Dakod <794470129@qq.com>
2019-07-01 10:43:08 +0800

    v0.0.6

---
+++ Elearning-common/src/main/java/org/darod/elearning/common/dto/UserModel.java
@@ -0,0 +1,42 @@
+package org.darod.elearning.common.dto;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Darod
+ * @version 1.0
+ * @date 2019/6/20 0020 9:20
+ */
+@Data
+public class UserModel {
+    private Integer id;
+    @NotBlank(message = "用户名不能为空")
+    private String name;
+    @NotNull(message = "性别不能为空")
+    private Byte gender;
+    @NotNull(message = "年龄不能为空")
+    @Min(value = 0,message = "年龄必须大于0岁")
+    @Max(value = 150,message = "年龄不能大于150岁")
+    private Integer age;
+    @NotBlank(message = "手机号不能为空")
+    private String telphone;
+    private String registerMode;
+    private String thirdPartyId;
+    @NotBlank(message = "密码不能为空")
+    @JSONField(serialize = false)
+    private String encryptPassword;
+
+    private String email;
+    @NotNull(message = "头像URL不能为空")
+    @URL(message = "URL格式不正确")
+    private String headUrl;
+    @NotNull(message = "用户状态不能为空")
+    private Byte userState;
+}
---
+++ Elearning-gateway/src/main/java/org/darod/elearning/gateway/serviceimpl/UserServiceImpl.java
@@ -0,0 +1,76 @@
+package org.darod.elearning.gateway.serviceimpl;
+
+import org.darod.elearning.common.dto.UserModel;
+import org.darod.elearning.common.exception.BusinessException;
+import org.darod.elearning.common.exception.EmException;
+import org.darod.elearning.common.service.user.UserService;
+import org.darod.elearning.common.validator.ValidationResult;
+import org.darod.elearning.common.validator.ValidatorImpl;
+import org.darod.elearning.gateway.dao.UserDOMapper;
+import org.darod.elearning.gateway.dao.UserPasswordDOMapper;
+import org.darod.elearning.gateway.dataobject.UserDO;
+import org.darod.elearning.gateway.dataobject.UserPasswordDO;
+import org.darod.elearning.gateway.utils.CopyPropertiesUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Darod
+ * @version 1.0
+ * @date 2019/6/20 0020 15:20
+ */
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDOMapper userDOMapper;
+    @Autowired
+    private UserPasswordDOMapper userPasswordDOMapper;
+    @Autowired
+    private ValidatorImpl validator;
+
+
+    @Override
+    public UserModel getUserById(int id) {
+        return null;
+    }
+
+    @Override
+    public void register(UserModel userModel) throws BusinessException {
+        if (userModel == null) {
+            throw new BusinessException(EmException.PARAMETER_VALIDATION_ERROR);
+        }
+//        if (StringUtils.isEmpty(userModel.getName()) || userModel.getAge() == null ||
+//                userModel.getGender() == null || StringUtils.isEmpty(userModel.getTelphone()) ||
+//                StringUtils.isEmpty(userModel.getEncryptPassword())) {
+//            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR);
+//        }
+        ValidationResult validationResult = validator.validate(userModel);
+        if (validationResult.isHasError()) {
+            throw new BusinessException(EmException.PARAMETER_VALIDATION_ERROR, validationResult.getErrMsg());
+        }
+        UserDO userDO = CopyPropertiesUtils.copyProperties(userModel,UserDO.class);
+        try {
+            userDOMapper.insertSelective(userDO);
+        } catch (DuplicateKeyException e) {
+            throw new BusinessException(EmException.PARAMETER_VALIDATION_ERROR, "手机号重复");
+        }
+        userModel.setId(userDO.getId());
+        UserPasswordDO userPasswordDO = new UserPasswordDO();
+        BeanUtils.copyProperties(userModel, userPasswordDO);
+        userPasswordDO.setUserId(userDO.getId());
+        userPasswordDOMapper.insertSelective(userPasswordDO); //selective 只会把不为null的字段插入数据库，为null的字段会是默认值
+    }
+
+    @Override
+    public UserModel validateLogin(String telphone, String encryptPassword) throws BusinessException {
+        return null;
+    }
+
+    @Override
+    public String hello() {
+        return "hello";
+    }
+}
